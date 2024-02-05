ce3bf4817846a8cdb80e3c239569ebe2f9d82cbb
Onurtaci <onurtaci1819@gmail.com>
2024-02-06 00:52:36 +0300

    OOP

---
+++ Intro/Business/CourseManager.cs
@@ -0,0 +1,43 @@
+﻿using Intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.Business
+{
+    public class CourseManager
+    {
+        Course[] courses = new Course[3];
+
+        //constructor
+        public CourseManager()
+        {
+            Course course1 = new();
+            course1.Id = 1;
+            course1.Name = "C#";
+            course1.Description = "C#  .NET 8";
+            course1.Price = 0;
+
+            Course course2 = new();
+            course2.Id = 2;
+            course2.Name = "Java";
+            course2.Description = "Java 17..";
+            course2.Price = 10;
+
+            Course course3 = new();
+            course3.Id = 3;
+            course3.Name = "Python";
+            course3.Description = "Python 3.12 ..";
+            course3.Price = 20;
+
+            courses = [course1, course2, course3];
+        }
+
+        public Course[] GetAll()
+        {
+            return courses;
+        }
+    }
+}
--- Intro/Program.cs
+++ Intro/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Intro.Business;
+using Intro.Entities;
+
+Console.WriteLine("Hello, World!");
 
 string message1 = "Krediler";
 int term = 12;
@@ -22,6 +25,39 @@ string[] loans = { "Konut Kredisi", "Taşıt Kredisi", "İhtiyaç Kredisi" }; //
 for (int i = 0; i < loans.Length; i++)
 {
     Console.WriteLine(loans[i]);
+}
+
+//Course course1 = new();
+//course1.Id = 1;
+//course1.Name = "C#";
+//course1.Description = "C#  .NET 8";
+//course1.Price = 0;
+
+//Course course2 = new();
+//course2.Id = 2;
+//course2.Name = "Java";
+//course2.Description = "Java 17..";
+//course2.Price = 10;
+
+//Course course3 = new();
+//course3.Id = 3;
+//course3.Name = "Python";
+//course3.Description = "Python 3.12 ..";
+//course3.Price = 20;
+
+//Course[] courses = { course1, course2, course3 };
+
+//foreach (var course in courses)
+//{
+//    Console.WriteLine(course.Name + " / " + course.Price);
+//}
+
+CourseManager courseManager = new();
+Course[] courses2 = courseManager.GetAll();
+
+foreach (var course in courses2)
+{
+    Console.WriteLine(course.Name + " / " + course.Price);
 }
 
 Console.WriteLine("Kod bitti");
